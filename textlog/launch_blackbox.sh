d4e25d38ad3d2ced47655af45456378366876c61
Robert Twomey <robert.twomey@gmail.com>
2019-08-29 05:20:21 +0100

    updated launch_blackboxsh

--- textlog/launch_blackbox.sh
+++ textlog/launch_blackbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 10
-cd /home/pi/housemachine/python
-/usr/bin/screen -dmS blackbox /usr/bin/python /home/pi/housemachine/python/blackbox_logger.py
+cd /home/pi/housemachine/textlog/
+/usr/bin/screen -dmS blackbox /usr/bin/python /home/pi/housemachine/textlog/blackbox_logger.py
