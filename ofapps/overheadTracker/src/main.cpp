45e0bde8b97bd56a830f95ad3d1a7fb62f5a002c
roberttwomey <robert.twomey@gmail.com>
2019-09-08 13:51:06 -0400

    tinkering with osx fullscreen

--- ofapps/overheadTracker/src/main.cpp
+++ ofapps/overheadTracker/src/main.cpp
@@ -3,8 +3,13 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(410, 615, OF_WINDOW);			// <-------- setup the GL context
+	// ofSetupOpenGL(400, 600, OF_WINDOW);			// <-------- setup the GL context
 
+	ofGLFWWindowSettings settings;
+	settings.setGLVersion(4, 1);
+	settings.multiMonitorFullScreen = true;
+	settings.windowMode = OF_FULLSCREEN;
+	ofCreateWindow(settings);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
